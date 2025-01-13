be8bb440820722dd8201c4820cbecbe5ea44959e
Annie-Chigana <chiganagana.a@gmail.com>
2025-01-13 11:38:37 +0200

    Variables and data types

---
+++ VaribaleAndDataTypes/src/variablesanddatatypes/VaribaleAndDataTypes.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variablesanddatatypes;
+/**
+ *
+ * @author annie
+ */
+public class VaribaleAndDataTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Primitive data Types
+        byte smallNumber = 100;
+        short shortNumber = 5000;
+        int normalNumber = 100000;
+        long largeNumber = 100000000L;
+        float decimalNumber = 10.5f;
+        double preciseDecimal = 20.12345;
+        char letter = 'A';
+        boolean isJavaFun = true;
+        
+        System.out.println("byte value: " + smallNumber);
+        System.out.println("byte value: " + shortNumber);
+        System.out.println("byte value: " + normalNumber);
+        System.out.println("byte value: " + largeNumber);
+        System.out.println("byte value: " + decimalNumber);
+        System.out.println("byte value: " + preciseDecimal);
+        System.out.println("byte value: " + letter);
+        System.out.println("byte value: " + isJavaFun);
+    }
+    
+}
